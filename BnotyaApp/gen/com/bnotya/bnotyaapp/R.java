aa789714a2f8c9a841146d10f95db8b6d0b70e68
MaximRikel <saevhern@gmail.com>
2014-02-21 09:39:44 +0200

    I've added UnitTest but can't run it

--- BnotyaApp/gen/com/bnotya/bnotyaapp/R.java
+++ BnotyaApp/gen/com/bnotya/bnotyaapp/R.java
@@ -997,8 +997,8 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_open_search=0x7f05003e;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_open_search=0x7f05003f;
+        public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1023,6 +1023,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int openWomenMenuButton=0x7f05003c;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1048,7 +1049,7 @@ containing a value of this type.
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
-        public static final int womenlist=0x7f05003c;
+        public static final int womenlist=0x7f05003d;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
---
+++ BnotyaApp/tests/src/com/bnotya/bnotyaapp/tests/LaunchActivityTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bnotya.bnotyaapp.tests;
+
+import com.bnotya.bnotyaapp.R;
+import com.bnotya.bnotyaapp.MainActivity;
+import com.bnotya.bnotyaapp.MainWomenActivity;
+
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.widget.Button;
+
+/**
+ * Tests LaunchActivity in isolation from the system.
+ */
+public class LaunchActivityTest extends ActivityUnitTestCase<MainActivity> 
+{
+    private Intent _launchIntent;
+
+    public LaunchActivityTest() 
+    {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception 
+    {
+        super.setUp();
+        //Create an intent to launch target Activity
+        _launchIntent = new Intent(getInstrumentation().getTargetContext(),
+        		MainActivity.class);
+    }
+
+    /**
+     * Tests the preconditions of this test fixture.
+     */
+    @MediumTest
+    public void testPreconditions() 
+    {
+        //Start the activity under test in isolation, without values for savedInstanceState and
+        //lastNonConfigurationInstance
+        startActivity(_launchIntent, null, null);
+        final Button openWomenMenuButton = (Button) getActivity().findViewById(R.id.openWomenMenuButton);
+
+        assertNotNull("MainActivity is null", getActivity());
+        assertNotNull("openWomenMenuButton is null", openWomenMenuButton);
+    }
+
+    @MediumTest
+    public void testLaunchNextActivityButton_labelText() 
+    {
+        startActivity(_launchIntent, null, null);
+        final Button openWomenMenuButton = (Button) getActivity().findViewById(R.id.openWomenMenuButton);
+
+        final String expectedButtonText = getActivity().getString(R.string.open_women_menu);
+        assertEquals("Unexpected button label text", expectedButtonText,
+        		openWomenMenuButton.getText());
+    }
+
+    @MediumTest
+    public void testNextActivityWasLaunchedWithIntent() 
+    {
+        startActivity(_launchIntent, null, null);
+        final Button openWomenMenuButton = (Button) getActivity().findViewById(R.id.openWomenMenuButton);
+        //Because this is an isolated ActivityUnitTestCase we have to directly click the
+        //button from code
+        openWomenMenuButton.performClick();
+
+        // Get the intent for the next started activity
+        final Intent launchIntent = getStartedActivityIntent();
+        //Verify the intent was not null.
+        assertNotNull("Intent was null", launchIntent);
+        //Verify that LaunchActivity was finished after button click
+        assertTrue(isFinishCalled());
+
+
+        final String payload = launchIntent.getStringExtra(NextActivity.EXTRAS_PAYLOAD_KEY);
+        //Verify that payload data was added to the intent
+        assertEquals("Payload is empty", LaunchActivity.STRING_PAYLOAD
+                , payload);
+    }
+}
